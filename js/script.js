9a3d168623a192924516bb17f79814dfd5e508f4
Mauricio <mauricio.vaz@aluno.ifsp.edu>
2024-12-04 14:31:25 -0300

    Site final

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Função para abrir o modal
+function openLoginModal() {
+    document.getElementById("loginModal").style.display = "flex";
+  }
+  
+  // Função para fechar o modal
+  function closeLoginModal() {
+    document.getElementById("loginModal").style.display = "none";
+  }
